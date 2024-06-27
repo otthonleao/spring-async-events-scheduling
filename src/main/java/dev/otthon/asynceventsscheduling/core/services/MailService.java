c1798284719063eeb6c1515920995a91ef9565b3
Otthon Leão <otthonleao@hotmail.com>
2024-06-27 11:51:11 -0400

    feat: add `@Asynx` and change execution default

--- src/main/java/dev/otthon/asynceventsscheduling/core/services/MailService.java
+++ src/main/java/dev/otthon/asynceventsscheduling/core/services/MailService.java
@@ -2,6 +2,7 @@ package dev.otthon.asynceventsscheduling.core.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class MailService {
 
     private final Logger log = LoggerFactory.getLogger(MailService.class);
 
+    @Async // Para executar de maneira assincrona em uma thread separada
     public void send(String to, String subject, String body) {
         // Simulando o DELAY do processo de esperar o envio de email ser realizado para depois enviar a response
         try {
